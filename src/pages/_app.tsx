caa9b6089ca844bed006e60261e020fb983be075
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-02-17 12:46:50 +0700

    style(layout): :art: test setting max-scale

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,7 +13,7 @@ const MyApp = ({ Component, pageProps }) => {
       <Head>
         <meta
           name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+          content="minimum-scale=1, maximum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
         />
       </Head>
       <Layout>
