4dcdb3d8ea961b18e9f2f16ad04d79615d006a13
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-02-23 01:32:14 +0700

    feat(auth): add emailVerified checking

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,100 @@
-import { Box } from "@chakra-ui/react";
-import { AuthContext } from "components/auth/AuthProvider";
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  Box,
+  Grid,
+  Link,
+  useToast,
+} from "@chakra-ui/react";
+import { useContext, useEffect, useState } from "react";
 
 import HallWrapper from "components/hall";
-import { useContext, useState } from "react";
+
+import { AuthContext } from "components/auth/AuthProvider";
+import { requestVerificationMail } from "functions/firebase/auth";
+import SpokerLoading from "components/ui/SpokerLoading";
 
 const Home = () => {
   const { currentUser } = useContext(AuthContext);
   const [name, setName] = useState<string>();
   const [busy, setBusy] = useState<boolean>(true);
   const [isEmailVerified, setIsEmailVerified] = useState<boolean>(false);
+  const toast = useToast();
+
+  const requestEmailVerification = () => {
+    currentUser.reload().then(() => {
+      if (currentUser.emailVerified) {
+        setIsEmailVerified(currentUser.emailVerified);
+        toast({
+          description: `Your email is already verified.`,
+          status: "info",
+          position: "top",
+          isClosable: true,
+        });
+      } else {
+        requestVerificationMail()
+          .then(() => {
+            toast({
+              title: "Verification Requested",
+              description: `Please check your email (${currentUser.email}).`,
+              status: "info",
+              position: "top",
+              isClosable: true,
+            });
+          })
+          .catch((err) => {
+            toast({
+              description: err.message,
+              status: "warning",
+              position: "top",
+              isClosable: true,
+            });
+          });
+      }
+    });
+  };
+
+  useEffect(() => {
+    setBusy(true);
+    if (currentUser) {
+      currentUser.reload().then(() => {
+        setName(currentUser.displayName);
+        setIsEmailVerified(currentUser.emailVerified);
+        setBusy(false);
+      });
+    } else {
+      setBusy(false);
+    }
+  }, [currentUser]);
+
+  if (busy) {
+    return <SpokerLoading />;
+  }
 
   return (
     <Box mb={8} w="full">
-      <HallWrapper />
+      {currentUser && !isEmailVerified ? (
+        <Alert borderRadius={24} status="warning">
+          <AlertIcon />
+          <Grid>
+            <AlertTitle>
+              Your email is not verified yet. Please check your email for
+              verification instructions.
+            </AlertTitle>
+
+            <AlertDescription>
+              Haven't received any verification email?{" "}
+              <Link fontWeight="semibold" onClick={requestEmailVerification}>
+                Request Verification Link
+              </Link>
+            </AlertDescription>
+          </Grid>
+        </Alert>
+      ) : (
+        <HallWrapper />
+      )}
     </Box>
   );
 };
