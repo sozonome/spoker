ec588c9ba9ce813580d45c21345e7e2d374fcf13
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-04-09 19:35:32 +0700

    chore: add firebase rules constructor

--- src/constants/hideLabel.ts
+++ src/constants/hideLabel.ts
@@ -1,5 +1,3 @@
-import { ReactNode } from "react";
-
 export enum HideLabelOptions {
   monkey,
   chicken,
---
+++ src/functions/firebase/rules.ts
@@ -0,0 +1,68 @@
+import {
+  auth,
+  between,
+  equal,
+  newData,
+  node,
+  param,
+  props,
+  validate,
+  write,
+} from "@jahed/firebase-rules";
+
+export const rules = {
+  rules: node(
+    props({
+      rooms: node(
+        param("$roomID", () =>
+          node(
+            props({
+              config: node(
+                props({
+                  hideLabel: node(
+                    validate(
+                      newData.isString((newVal) =>
+                        newVal.matches(/monkey|chicken|cow|fish/)
+                      )
+                    )
+                  ),
+                  isFreezeAfterVote: node(validate(newData.isBoolean())),
+                })
+              ),
+              room: node(
+                props({
+                  isPrivate: node(validate(newData.isBoolean())),
+                  name: node(validate(newData.isString())),
+                  password: node(validate(newData.isString())),
+                })
+              ),
+              task: node(
+                props({
+                  description: node(validate(newData.isString())),
+                  name: node(validate(newData.isString())),
+                })
+              ),
+              users: node(
+                param("$userID", ($userID) =>
+                  node(
+                    props({
+                      name: node(validate(newData.isString())),
+                      role: node(validate(newData.isString())),
+                      point: node(
+                        validate(
+                          newData.isNumber((newVal) => between(newVal, -1, 101))
+                        )
+                      ),
+                    }),
+                    write(equal($userID, auth.uid)),
+                    validate(newData.hasChildren(["name", "role"]))
+                  )
+                )
+              ),
+            }),
+          ),
+        )
+      ),
+    })
+  ),
+};
