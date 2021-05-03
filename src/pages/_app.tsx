8603cf73061e4c3250098a292ce271abfed0119c
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-05-03 23:15:21 +0700

    feat(analytics): add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,41 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import Head from "next/head";
+import { useEffect } from "react";
 import "@fontsource/jost/latin.css";
 import "@fontsource/inter/latin.css";
 
 import Layout from "components/layout";
 import { AuthProvider } from "components/auth/AuthProvider";
 
+import { initGA, logPageView } from "lib/analytics";
+
 import customTheme from "styles/customTheme";
 import "styles/globals.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    initGA();
+    // `routeChangeComplete` won't run for the first page load unless the query string is
+    // hydrated later on, so here we log a page view if this is the first render and
+    // there's no query string
+    if (!router.asPath.includes("?")) {
+      logPageView();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    // Listen for page changes after a navigation or when the query changes
+    router.events.on("routeChangeComplete", logPageView);
+    return () => {
+      router.events.off("routeChangeComplete", logPageView);
+    };
+  }, [router.events]);
+
   return (
     <ChakraProvider theme={customTheme}>
       <Head>
