4db0e4a640911cfa0b5bdfe83ae1210a4e25be52
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-06-15 01:39:47 +0700

    refactor(hall): remove unused var and comments

--- src/components/hall/components/CreateRoom.tsx
+++ src/components/hall/components/CreateRoom.tsx
@@ -16,42 +16,35 @@ import { CreateRoomFormSchema, CreateRoomFormType } from "../types";
 const CreateRoom = () => {
   const toast = useToast();
   const router = useRouter();
-  const {
-    values,
-    errors,
-    dirty,
-    handleChange,
-    handleSubmit,
-  } = useFormik<CreateRoomFormType>({
-    initialValues: {
-      name: "",
-      id: nanoid(21),
-      isPrivate: false,
-      password: "",
-    },
-    validationSchema: CreateRoomFormSchema,
-    onSubmit: async (formValues: CreateRoomFormType) => {
-      setIsLoading(true);
-      await createRoom(formValues).then((res) => {
-        setIsLoading(false);
-        if (res !== true && res.message) {
-          toast({
-            position: "top-right",
-            title: "Create Room Fail",
-            description: res.message,
-            status: "error",
-            isClosable: true,
-          });
-        } else {
-          router.push(`/join/${formValues.id}`);
-        }
-      });
-    },
-  });
-  const { name, id, isPrivate, password } = values;
-  // const [isPasswordShown, setIsPasswordShown] = useState<boolean>(false);
+  const { values, errors, dirty, handleChange, handleSubmit } =
+    useFormik<CreateRoomFormType>({
+      initialValues: {
+        name: "",
+        id: nanoid(21),
+        isPrivate: false,
+        password: "",
+      },
+      validationSchema: CreateRoomFormSchema,
+      onSubmit: async (formValues: CreateRoomFormType) => {
+        setIsLoading(true);
+        await createRoom(formValues).then((res) => {
+          setIsLoading(false);
+          if (res !== true && res.message) {
+            toast({
+              position: "top-right",
+              title: "Create Room Fail",
+              description: res.message,
+              status: "error",
+              isClosable: true,
+            });
+          } else {
+            router.push(`/join/${formValues.id}`);
+          }
+        });
+      },
+    });
+  const { name, id } = values;
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  // const { size: _, ...contraInputStyleCompact } = contraInputStyle;
 
   return (
     <SpokerWrapperGrid gap={8}>
@@ -76,44 +69,6 @@ const CreateRoom = () => {
           isInvalid={(errors?.id?.length ?? 0) > 0}
           helperText={errors.id}
         />
-
-        {/* <FormControl display="flex" alignItems="center" gridGap={2}>
-          <Switch
-            name="isPrivate"
-            isChecked={isPrivate}
-            onChange={handleChange}
-          />
-          <FormLabel>private</FormLabel>
-          <InputGroup size="md">
-            <Input
-              disabled={!isPrivate}
-              onChange={handleChange}
-              name="password"
-              value={password}
-              type={isPrivate && isPasswordShown ? "text" : "password"}
-              placeholder="room password"
-              size="md"
-              {...contraInputStyleCompact}
-            />
-            <InputRightElement
-              children={
-                <IconButton
-                  size="sm"
-                  aria-label="show-password"
-                  disabled={!isPrivate}
-                  icon={
-                    isPrivate && isPasswordShown ? (
-                      <AiFillEye />
-                    ) : (
-                      <AiFillEyeInvisible />
-                    )
-                  }
-                  onClick={() => setIsPasswordShown(!isPasswordShown)}
-                />
-              }
-            />
-          </InputGroup>
-        </FormControl> */}
       </Grid>
 
       <Button
