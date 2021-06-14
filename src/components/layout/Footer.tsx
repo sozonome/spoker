00c93f10d120117cf2bd0f9f84ef2b6e36654251
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-06-15 02:13:54 +0700

    style(layout): add repo url link to footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,4 +1,6 @@
 import { Flex, Link, Text } from "@chakra-ui/layout";
+import { RiGithubFill } from "react-icons/ri";
+import { version, repository } from "../../../package.json";
 
 const Footer = () => {
   return (
@@ -9,6 +11,13 @@ const Footer = () => {
           sznm.dev
         </Link>
       </Text>
+
+      <Flex marginLeft="auto" alignItems="center" gridGap={2}>
+        <Link href={`${repository.url}/blob/main/CHANGELOG.md`} isExternal>v{version}</Link>
+        <Link href={repository.url} isExternal>
+          <RiGithubFill fontSize="2rem" />
+        </Link>
+      </Flex>
     </Flex>
   );
 };
