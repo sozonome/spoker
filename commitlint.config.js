18b3a45532f4e7933006597b5eccc0610fbbca77
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-08-21 20:40:09 +0700

    chore: update packages, setup commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+/** @type {import('@commitlint/types').UserConfig} */
+
+const CommitLintConfiguration = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-enum": [
+      2,
+      "always",
+      [
+        "hall",
+        "room",
+        "auth",
+        "layout",
+        "ui",
+        "pages",
+        "storybook",
+        "functions",
+        "types",
+      ],
+    ],
+    "scope-case": [2, "always", "kebab-case"],
+  },
+};
+
+module.exports = CommitLintConfiguration;
