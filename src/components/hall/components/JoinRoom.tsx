2c670f7359fef6a9e444117d7c699f222cbe80eb
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-08-26 22:50:46 +0700

    refactor(hall): :recycle: convert anon func and change button onClick to Link

--- src/components/hall/components/JoinRoom.tsx
+++ src/components/hall/components/JoinRoom.tsx
@@ -1,14 +1,16 @@
 import { Button } from "@chakra-ui/button";
 import { Grid, Heading } from "@chakra-ui/layout";
-import { useRouter } from "next/router";
-import { useState } from "react";
+import Link from "next/link";
+import { ChangeEventHandler, useState } from "react";
 
 import SpokerInput from "components/ui/SpokerInput";
 import SpokerWrapperGrid from "components/ui/SpokerWrapperGrid";
 
 const JoinRoom = () => {
-  const router = useRouter();
   const [roomId, setRoomId] = useState<string>("");
+
+  const handleChangeInput: ChangeEventHandler<HTMLInputElement> = (e) =>
+    setRoomId(e.target.value);
 
   return (
     <SpokerWrapperGrid gap={8} backgroundColor="orange.500" color="white">
@@ -18,20 +20,23 @@ const JoinRoom = () => {
         <SpokerInput
           label="Room ID"
           value={roomId}
-          onChange={(e) => setRoomId(e.target.value)}
+          onChange={handleChangeInput}
           placeholder="quick-brown-fox"
           _placeholder={{ color: "orange.200" }}
         />
       </Grid>
 
-      <Button
-        alignSelf="flex-end"
-        backgroundColor="black"
-        _hover={{ backgroundColor: "orange.400" }}
-        onClick={() => router.push(`/join/${roomId}`)}
-      >
-        Let Me in!
-      </Button>
+      <Link href={`/join/${roomId}`} passHref>
+        <Button
+          as="a"
+          disabled={roomId.length === 0}
+          alignSelf="flex-end"
+          backgroundColor="black"
+          _hover={{ backgroundColor: "orange.400" }}
+        >
+          Let Me in!
+        </Button>
+      </Link>
     </SpokerWrapperGrid>
   );
 };
