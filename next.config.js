3000c56c58c2f19dda6ecfaac2099e8868807e58
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-10-22 18:27:46 +0700

    Revert "chore: use preact for production build (#27)"
    This reverts commit a227ca7aa0bd1ac2e0e098f0e667580acbb67c95.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 const withPWA = require("next-pwa");
 
-/** @type {import('next').NextConfig} */
 module.exports = withPWA({
   pwa: {
     disable:
@@ -12,17 +11,5 @@ module.exports = withPWA({
       // to re-generate manifest.json, you can visit https://tomitm.github.io/appmanifest/
     dest: "public",
     register: true,
-  },
-  webpack: (config, { dev, isServer }) => {
-    // Replace React with Preact only in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      });
-    }
-
-    return config;
   },
 });
