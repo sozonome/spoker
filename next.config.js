a227ca7aa0bd1ac2e0e098f0e667580acbb67c95
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-10-22 17:14:10 +0700

    chore: use preact for production build (#27)

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const withPWA = require("next-pwa");
 
+/** @type {import('next').NextConfig} */
 module.exports = withPWA({
   pwa: {
     disable:
@@ -11,5 +12,17 @@ module.exports = withPWA({
       // to re-generate manifest.json, you can visit https://tomitm.github.io/appmanifest/
     dest: "public",
     register: true,
+  },
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+
+    return config;
   },
 });
