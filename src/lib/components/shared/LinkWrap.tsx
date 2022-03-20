3373b58b2ff62e3793453e3e996b1560d6832ce0
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-03-20 09:59:50 +0700

    fix(hall): make next link switchable for disable state

---
+++ src/lib/components/shared/LinkWrap.tsx
@@ -0,0 +1,16 @@
+import type { LinkProps } from "next/link";
+import Link from "next/link";
+
+type LinkWrapProps = React.PropsWithChildren<LinkProps> & {
+  isDisabled?: boolean;
+};
+
+const LinkWrap = ({ isDisabled, ...linkProps }: LinkWrapProps) => {
+  if (isDisabled) {
+    return linkProps.children as React.ReactElement;
+  }
+
+  return <Link {...linkProps} />;
+};
+
+export default LinkWrap;
