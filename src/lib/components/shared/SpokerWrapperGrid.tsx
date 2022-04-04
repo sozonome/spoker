bab05a7b0375f14a2103d25d142715d771138b7f
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-04-05 02:06:32 +0700

    feat: add modal wrapper

--- src/lib/components/shared/SpokerWrapperGrid.tsx
+++ src/lib/components/shared/SpokerWrapperGrid.tsx
@@ -1,22 +1,15 @@
 import type { GridProps } from "@chakra-ui/react";
 import { Grid, useColorModeValue } from "@chakra-ui/react";
 
+import { contraGridStyle } from "./style";
+
 type SpokerWrapperGridProps = GridProps;
 
 const SpokerWrapperGrid = ({ children, ...props }: SpokerWrapperGridProps) => {
   const backgroundColor = useColorModeValue(undefined, "gray.700");
 
-  const contraGridStyle: Partial<GridProps> = {
-    padding: 4,
-    borderRadius: 16,
-    borderWidth: `2px`,
-    borderColor: "black",
-    boxShadow: `0px 6px 0px black`,
-    backgroundColor,
-  };
-
   return (
-    <Grid {...contraGridStyle} {...props}>
+    <Grid {...contraGridStyle} backgroundColor={backgroundColor} {...props}>
       {children}
     </Grid>
   );
