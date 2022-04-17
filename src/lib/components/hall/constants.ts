05b8425ad908f0972101a0e63a1c2b248af082cb
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-04-17 12:44:44 +0700

    refactor(hall): switch formik to rhf

---
+++ src/lib/components/hall/constants.ts
@@ -0,0 +1,28 @@
+import { nanoid } from "nanoid";
+import * as yup from "yup";
+
+import { ALLOWED_CHAR_CHECK } from "lib/constants/allowedValues";
+
+import type { CreateRoomFormType } from "./types";
+
+const randomId = nanoid(21);
+
+export const initialValues: CreateRoomFormType = {
+  name: "",
+  id: randomId,
+  isPrivate: false,
+  password: "",
+};
+
+export const createRoomFormSchema = yup.object().shape({
+  name: yup.string().required().defined(),
+  id: yup
+    .string()
+    .matches(ALLOWED_CHAR_CHECK, {
+      message: "username contain unallowed character",
+    })
+    .required()
+    .defined(),
+  isPrivate: yup.boolean().defined(),
+  password: yup.string(),
+});
