238a3afe9c44afdd9f10aacb3d5b1c5c558c125d
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-05-10 00:14:51 +0700

    refactor: remove todo and remove switch in auth page

--- src/lib/pages/auth/index.tsx
+++ src/lib/pages/auth/index.tsx
@@ -1,9 +1,3 @@
-/**
- * @todo
- * [ ] handle mode "resetPassword"
- * [ ] handle mode "recoverEmail"
- */
-
 import { useToast } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import * as React from "react";
@@ -33,38 +27,36 @@ const Auth = () => {
   }, [router, toast]);
 
   const handleAuthCallback = React.useCallback(() => {
-    // eslint-disable-next-line sonarjs/no-small-switch
-    switch (mode) {
-      case "verifyEmail":
-        setIsProcessed(true);
-        handleVerifyEmail(oobCode as string)
-          .then(() => {
+    if (mode === "verifyEmail") {
+      setIsProcessed(true);
+      handleVerifyEmail(oobCode as string)
+        .then(() => {
+          toast({
+            title: "Email Verification Success",
+            status: "success",
+            position: "top",
+            isClosable: true,
+          });
+          if (currentUser) {
+            currentUser.reload().then(() => router.push("/"));
+          } else {
+            router.push("/");
+          }
+        })
+        .catch((err) => {
+          router.push("/").then(() => {
             toast({
-              title: "Email Verification Success",
-              status: "success",
+              description: err.message,
+              status: "error",
               position: "top",
               isClosable: true,
             });
-            if (currentUser) {
-              currentUser.reload().then(() => router.push("/"));
-            } else {
-              router.push("/");
-            }
-          })
-          .catch((err) => {
-            router.push("/").then(() => {
-              toast({
-                description: err.message,
-                status: "error",
-                position: "top",
-                isClosable: true,
-              });
-            });
           });
-        break;
-      default:
-        handleInvalidLink();
+        });
+      return;
     }
+
+    handleInvalidLink();
   }, [currentUser, handleInvalidLink, mode, oobCode, router, toast]);
 
   React.useEffect(() => {
