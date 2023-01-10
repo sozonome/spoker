b743382ba48c2a150064fe4c8eacdc00956fabd4
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-01-10 12:28:55 +0700

    refactor: use jsdelivr as twemoji base
    - problem: maxcdn down
    - ref: https://github.com/twitter/twemoji/issues/580

--- src/lib/components/shared/Twemoji.tsx
+++ src/lib/components/shared/Twemoji.tsx
@@ -1,5 +1,5 @@
 import { memo } from "react";
-// import twemoji from "twemoji";
+import twemoji from "twemoji";
 
 type TwemojiProps = {
   emoji: string;
@@ -8,16 +8,15 @@ type TwemojiProps = {
 const Twemoji = ({ emoji }: TwemojiProps) => {
   return (
     <span
-    // eslint-disable-next-line react/no-danger
-    // dangerouslySetInnerHTML={{
-    //   __html: twemoji.parse(emoji, {
-    //     folder: "svg",
-    //     ext: ".svg",
-    //   }),
-    // }}
-    >
-      {emoji}
-    </span>
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{
+        __html: twemoji.parse(emoji, {
+          folder: "svg",
+          ext: ".svg",
+          base: "https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/",
+        }),
+      }}
+    />
   );
 };
 
