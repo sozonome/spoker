37708654bec7121da559842c19951ee89c2a9257
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-03-03 00:12:16 +0700

    refactor(theme): adjust gray color palette

---
+++ src/lib/styles/theme/colors.ts
@@ -0,0 +1,16 @@
+import type { DeepPartial, Theme } from '@chakra-ui/react';
+
+export const colors: DeepPartial<Theme['colors']> = {
+  gray: {
+    50: '#e8e9e9',
+    100: '#d1d2d2',
+    200: '#a3a5a5',
+    300: '#747978',
+    400: '#464c4b',
+    500: '#181f1e',
+    600: '#131918',
+    700: '#0e1312',
+    800: '#0a0c0c',
+    900: '#050606',
+  },
+};
