4ac2c8eeaebf40d702374e3988185382ed9142a0
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-09-15 23:18:40 +0700

    chore: disable sentry for local

--- sentry.edge.config.js
+++ sentry.edge.config.js
@@ -3,13 +3,17 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 const APP_ENV = process.env.APP_ENV;
 
-Sentry.init({
-  dsn: SENTRY_DSN,
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1.0,
-  environment: APP_ENV,
-  // ...
-  // Note: if you want to override the automatic release value, do not set a
-  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
-});
+const isLocal = process.env.NODE_ENV === 'development';
+
+if (!isLocal) {
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1.0,
+    environment: APP_ENV,
+    // ...
+    // Note: if you want to override the automatic release value, do not set a
+    // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+    // that it will also get attached to your source maps
+  });
+}
