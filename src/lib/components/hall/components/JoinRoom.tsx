5bc08447e5dde65502f2e8ee09ee3914aa0f91ff
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-10-10 22:16:08 +0700

    fix(hall): adjust join room button color

--- src/lib/components/hall/components/JoinRoom.tsx
+++ src/lib/components/hall/components/JoinRoom.tsx
@@ -1,9 +1,9 @@
 import { Button, Grid, Heading, useColorModeValue } from '@chakra-ui/react';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import type { ChangeEventHandler } from 'react';
 import * as React from 'react';
 
-import LinkWrap from '~/lib/components/shared/LinkWrap';
 import SpokerInput from '~/lib/components/shared/SpokerInput';
 import SpokerWrapperGrid from '~/lib/components/shared/SpokerWrapperGrid';
 
@@ -40,17 +40,14 @@ const JoinRoom = () => {
         />
       </Grid>
 
-      <LinkWrap href={`/join/${roomId}`} isDisabled={isDisabled} passHref>
-        <Button
-          as="a"
-          isDisabled={isDisabled}
-          alignSelf="flex-end"
-          backgroundColor="black"
-          _hover={{ backgroundColor: 'orange.400' }}
-        >
-          Let Me in!
-        </Button>
-      </LinkWrap>
+      <Button
+        as={Link}
+        href={`/join/${roomId}`}
+        isDisabled={isDisabled}
+        alignSelf="flex-end"
+      >
+        Let Me in!
+      </Button>
     </SpokerWrapperGrid>
   );
 };
